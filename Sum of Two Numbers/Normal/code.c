c05f45d5556ec35d957a152513cbb68a23077332
Hardik-Singh04 <hardikmanderwal04@gmail.com>
2021-10-08 11:20:20 +0530

    added code

---
+++ Sum of Two Numbers/Normal/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a,b,sum=0;
+
+    printf("Enter first number :");
+    scanf("%d",&a);
+
+    printf("Enter second number :");
+    scanf("%d",&b);
+
+    sum=a+b;
+
+    printf("Sum of a and b is :  %d\n",sum);
+
+    return 0;
+}
