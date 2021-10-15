e329c2bf177b75d10ffc19719d3e7cf51a9b12e4
Hardik-Singh04 <hardikmanderwal04@gmail.com>
2021-10-16 03:49:30 +0530

    added code

---
+++ Multiplication of Two Numbers/Normal/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a,b,multi=0;
+
+    printf("Enter first number :");
+    scanf("%d",&a);
+
+    printf("Enter second number :");
+    scanf("%d",&b);
+
+    multi=a*b;
+
+    printf("Multiplication of a and b is : %d",multi);
+    
+    return 0;
+}
